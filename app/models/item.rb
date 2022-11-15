0795fd5b14b7ac02495bc04b84383990d2488812
miho0434 <techstudy0434@gmail.com>
2022-11-15 13:52:05 +0900

    バリデーションの修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,10 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id,                 numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_charge_burden_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id,            numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_day_id,          numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,             numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_day_id,           numericality: { other_than: 1, message: "can't be blank" }
   validates :price,                     presence: true,
-                                        numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は ¥300~9,999,999 の間で半角数字で指定してください" },
-                                        format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
-
+                                        numericality:{ only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は ¥300~9,999,999 の間で半角数字で指定してください" }
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
