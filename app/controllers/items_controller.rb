1bcba93b42e2b60b23eccfb79e3b2e3870d0025b
miho0434 <techstudy0434@gmail.com>
2022-11-17 19:55:51 +0900

    indexの中を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.includes(:user)
     @items = Item.order("id DESC")
  end
 
