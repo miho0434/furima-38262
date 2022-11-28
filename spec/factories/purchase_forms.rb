a94541c81b0de82ce20a8bcce35ab0ece5604374
miho0434 <techstudy0434@gmail.com>
2022-11-28 15:18:59 +0900

    外部キーに関して削除

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,13 +1,11 @@
 FactoryBot.define do
   factory :purchase_form do
-    postal_code { '123-4567' }
-    prefecture_id { 3 }
-    city { '東京都' }
-    street_number { '1-1' }
-    building_name { '東京ハイツ' }
-    phone_number { '09012345678' }
-    user_id { 3 }
-    item_id { 3 }
-    token { 'tok_abcdefghijk00000000000000000' }
+    postal_code    { '123-4567' }
+    prefecture_id  { 3 }
+    city           { '東京都' }
+    street_number  { '1-1' }
+    building_name  { '東京ハイツ' }
+    phone_number   { '09012345678' }
+    token          { 'tok_abcdefghijk00000000000000000' }
   end
 end
